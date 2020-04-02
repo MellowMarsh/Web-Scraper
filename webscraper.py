e82eb166a8c6396225f9c436e0a654eb2f373142
Marshalla R <34112391+MellowMarsh@users.noreply.github.com>
2020-04-02 18:24:48 -0500

    added notes

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,9 @@
+
 import requests
 from bs4 import BeautifulSoup
-
+#pulling all homepage content to test out the web-scraper
 url = 'http://mothertreewellness.com'
 response = requests.get(url, timeout = 5)
 content = BeautifulSoup( response.content, "html.parser")
 
 print (content)
-
